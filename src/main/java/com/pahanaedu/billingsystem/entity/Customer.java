26ebb533d599083baf51253c8326b434b47e5867
vishnuka <vishnukayahan84@gmail.com>
2025-08-19 13:22:01 +0530

    add the attributes and other behavior

--- src/main/java/com/pahanaedu/billingsystem/entity/Customer.java
+++ src/main/java/com/pahanaedu/billingsystem/entity/Customer.java
@@ -6,5 +6,79 @@ package com.pahanaedu.billingsystem.entity;
  * Date:2025-08-16
  * Time:12:57
  */
-public class Customer {
+public class Customer implements SuperEntity{
+    private String accountNumber;
+    private String fullName;
+    private String address;
+    private String phoneNumber;
+    private int uniteConsumed;
+
+    public Customer() {
+    }
+
+    public Customer(String fullName, String address, String phoneNumber, int uniteConsumed) {
+        this.fullName = fullName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.uniteConsumed = uniteConsumed;
+    }
+
+    public Customer(String accountNumber, String fullName, String address, String phoneNumber, int uniteConsumed) {
+        this.accountNumber = accountNumber;
+        this.fullName = fullName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.uniteConsumed = uniteConsumed;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getUniteConsumed() {
+        return uniteConsumed;
+    }
+
+    public void setUniteConsumed(int uniteConsumed) {
+        this.uniteConsumed = uniteConsumed;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", address='" + address + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", uniteConsumed=" + uniteConsumed +
+                '}';
+    }
 }
--- src/main/java/com/pahanaedu/billingsystem/entity/Item.java
+++ src/main/java/com/pahanaedu/billingsystem/entity/Item.java
@@ -6,5 +6,70 @@ package com.pahanaedu.billingsystem.entity;
  * Date:2025-08-16
  * Time:12:57
  */
-public class Item {
+public class Item  implements SuperEntity{
+    private String itemCode;
+    private String description;
+    private Double unitPrice;
+    private int qtyOnHand;
+
+
+    //cons
+    public Item() {
+    }
+
+    public Item(String itemCode, String description, Double unitPrice, int qtyOnHand) {
+        this.itemCode = itemCode;
+        this.description = description;
+        this.unitPrice = unitPrice;
+        this.qtyOnHand = qtyOnHand;
+    }
+
+
+    //getter method
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    //setter method
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(Double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getQtyOnHand() {
+        return qtyOnHand;
+    }
+
+    public void setQtyOnHand(int qtyOnHand) {
+        this.qtyOnHand = qtyOnHand;
+    }
+
+
+    //tostring
+    @Override
+    public String toString() {
+        return "Item{" +
+                "itemCode='" + itemCode + '\'' +
+                ", description='" + description + '\'' +
+                ", unitPrice=" + unitPrice +
+                ", qtyOnHand=" + qtyOnHand +
+                '}';
+    }
+
 }
