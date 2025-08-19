421177ae5fea87fa76d11202203dc97d30662d8c
vishnuka <vishnukayahan84@gmail.com>
2025-08-19 13:21:17 +0530

    add the supper entity interface

---
+++ src/main/java/com/pahanaedu/billingsystem/entity/SuperEntity.java
@@ -0,0 +1,11 @@
+package com.pahanaedu.billingsystem.entity;
+
+/**
+ * Author: Vishnuka Yahan De Silva
+ * User:macbookair
+ * Date:2025-08-16
+ * Time:13:16
+ */
+
+public interface SuperEntity {
+}
