cedbbf9eb03732d39c851f19559befaa405fc605
vishnuka <vishnukayahan84@gmail.com>
2025-08-19 12:58:59 +0530

    add the entity class

---
+++ src/main/java/com/pahanaedu/billingsystem/dto/CustomerDTO.java
@@ -0,0 +1,8 @@
+package com.pahanaedu.billingsystem.dto;/**
+ * Author: Vishnuka Yahan De Silva
+ * User:macbookair
+ * Date:2025-08-19
+ * Time:12:57
+ */
+public class CustomerDTO {
+}
---
+++ src/main/java/com/pahanaedu/billingsystem/entity/Customer.java
@@ -0,0 +1,10 @@
+package com.pahanaedu.billingsystem.entity;
+
+/**
+ * Author: Vishnuka Yahan De Silva
+ * User:macbookair
+ * Date:2025-08-16
+ * Time:12:57
+ */
+public class Customer {
+}
